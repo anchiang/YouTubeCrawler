712f612483af8a233183a07f2fca594632613e07
anchiang <anchiang@gmail.com>
2017-06-04 20:18:22 +0800

    add setlocate

--- tutorial1.py
+++ tutorial1.py
@@ -3,13 +3,20 @@
 
 import urllib.request
 from bs4 import BeautifulSoup as bs 
+import locale
 
-url = "https://www.youtube.com/user/chuchushoeTW"
+locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
+
+url = "https://www.youtube.com/user/chuchushoeTW/about"
 response = urllib.request.urlopen(url) #開連結 
 html_cont = response.read() 
 soup = bs(html_cont,'html.parser',from_encoding='UTF-8') #結構化 
+
+res = soup.find_all(class_="about-stat")
+for line in res[0:2]:
+    print(line.b)
 '''
-results = soup.find_all("www.youtube.com\/yt\/")
-for line in results:
+people = locale.atoi(res[0].find('b').string)
+views = locale.atoi(res[1].find('b').string)
+print(n, people, views)
 '''
-print(soup.link)
